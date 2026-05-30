926159fb655fe8371f90cd4e676bb555ef40ed71
rishabhpandey106 <rishabhpandey230@gmail.com>
2026-05-30 12:56:52 +0530

    fix: api response

--- src/app/api/analytics/route.ts
+++ src/app/api/analytics/route.ts
@@ -43,9 +43,7 @@ export async function GET() {
         return NextResponse.json({
             "Unique Visitors": stats.visitors > stats.visits ? stats.visitors : stats.visits,
             "Total Views": stats.pageviews,
-            "Bounce Rate": stats.bounces,
-            "Visit Duration": Math.round(stats.totaltime * 40 / 1000) + "s",
-            "Average Visit Duration": Math.round(stats.totaltime / stats.visitors * 40 / 1000) + "s",
+            "Bounce Rate": Math.round(stats.bounces * 100 / stats.pageviews) + "%",
             "Time Range": `${new Date(startAt).toLocaleDateString()} - ${new Date(endAt).toLocaleDateString()}, 1 year`,
         });
     } catch (e) {
